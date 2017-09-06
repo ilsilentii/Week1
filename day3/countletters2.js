42a042da5fa9158500955e518dbba294611b105e
ilsilentii <travis.vdm@outlook.com>
2017-09-06 14:30:10 -0600

    revised code

--- day3/countletters2.js
+++ day3/countletters2.js
@@ -1,21 +1,14 @@
 function indexof(string) {
   var object = {};
-  string = string.split("").filter(function(str) {
-    return /\S/.test(str);
-   });
-
   for (i = 0; i < string.length; i++) {
     var character = string[i];
     if (character in object) {
-
       object[character].push(i);
-
-
     } else {
     object[character] = [i];
     }
  }
-  return object;
+    return object;
 }
 
 var myArgs = (process.argv.slice(2));
