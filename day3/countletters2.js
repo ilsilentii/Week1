4af5cddb4722efae9fd85121422432db51eace95
ilsilentii <travis.vdm@outlook.com>
2017-09-06 14:23:50 -0600

    added character position

---
+++ day3/countletters2.js
@@ -0,0 +1,29 @@
+function indexof(string) {
+  var object = {};
+  string = string.split("").filter(function(str) {
+    return /\S/.test(str);
+   });
+
+  for (i = 0; i < string.length; i++) {
+    var character = string[i];
+    if (character in object) {
+
+      object[character].push(i);
+
+
+    } else {
+    object[character] = [i];
+    }
+ }
+  return object;
+}
+
+var myArgs = (process.argv.slice(2));
+var input = myArgs.join("").replace(/\s/g, "");
+console.log(indexof(input));
+
+//console.log(indexof("lighthouse in the house"));
+
+
+
+
